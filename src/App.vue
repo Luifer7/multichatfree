65307d696199ab5e0140fa771f67990e9d3b67ef
luifer7 <corderoluisfernando7@gmail.com>
2022-12-08 17:48:43 -0500

    wtf wtf wtf

--- src/App.vue
+++ src/App.vue
@@ -10,8 +10,7 @@ const useData = useDataStore()
 
   <div class="box-nav d-flex align-items-center justify-content-between">
     <span class="fw-bold text-dark m-2 h4">MultichatFree</span>
-    <small class="infoo" >Si no completan el perfil no podran chatear si les generó error el primero user, hagan uno nuevo mientras yo areglo a experiencia de usuario, gracias.</small>
-
+    
     <ProfileComponent></ProfileComponent>
   
   </div>
